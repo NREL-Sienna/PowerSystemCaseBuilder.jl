6a5af6184ac32fed992ec5d8b219a416a2555c47
sourabhdalvi <sourabh.p.dalvi@gmail.com>
2021-02-04 10:35:44 -0700

    adding a comment to hybrid cost

--- data/data_5bus_pu.jl
+++ data/data_5bus_pu.jl
@@ -913,9 +913,10 @@ ORDC_cost_ts = [
     TimeSeries.TimeArray(DayAhead + Day(1), repeat([ORDC_cost], 24)),
 ]
 
+# TODO: add a sensible cost for hybrid devices
 hybrid_cost_ts = [
-    TimeSeries.TimeArray(DayAhead, repeat([999.0], 24)),
-    TimeSeries.TimeArray(DayAhead + Day(1), repeat([999.0], 24)),
+    TimeSeries.TimeArray(DayAhead, repeat([45.0], 24)),
+    TimeSeries.TimeArray(DayAhead + Day(1), repeat([45.0], 24)),
 ]
 
 Reserve_ts = [TimeSeries.TimeArray(DayAhead, rand(24)), TimeSeries.TimeArray(DayAhead + Day(1), rand(24))]
