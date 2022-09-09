d9bc19a5c7f766794343c87e286b770e015370a8
Rodrigo Henriquez <rodrigomha@gmail.com>
2022-09-08 18:56:20 -0700

    add more definitions

--- src/definitions.jl
+++ src/definitions.jl
@@ -21,8 +21,9 @@ const SEARCH_DIRS = [
 const SERIALIZE_FILE_EXTENSIONS =
     [".json", "_validation_descriptors.json", "_time_series_storage.h5"]
 
+const ACCEPTED_PSID_TEST_SYSTEMS_KWARGS = [:avr_type, :tg_type, :pss_type, :gen_type]
 const AVAILABLE_PSID_PSSE_AVRS_TEST =
-    ["AC1A", "AC1A_SAT", "EXAC1", "EXST1", "SEXS", "SEXS_noTe"]
+    ["AC1A", "AC1A_SAT", "EXAC1", "EXST1", "SEXS", "SEXS_noTE"]
 
 const AVAILABLE_PSID_PSSE_TGS_TEST = ["GAST", "HYGOV", "TGOV1"]
 
