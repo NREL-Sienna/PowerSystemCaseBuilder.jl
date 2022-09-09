ead5a18714676df59d2666b8c1f623953948aead
Jose Daniel Lara <jdlara@berkeley.edu>
2022-09-09 15:14:36 -0700

    [WIP] fix tests

---
+++ test/test_psidtestsystems.jl
@@ -0,0 +1,45 @@
+PSID_BUILD_TESTS = Dict(
+    "psid_psse_test_avr" => (:avr_type, PSB.AVAILABLE_PSID_PSSE_AVRS_TEST),
+    "psid_psse_test_tg" => (:tg_type, PSB.AVAILABLE_PSID_PSSE_TGS_TEST),
+    "psid_psse_test_gen" => (:gen_type, PSB.AVAILABLE_PSID_PSSE_GENS_TEST),
+    "psid_psse_test_pss" => (:pss_type, PSB.AVAILABLE_PSID_PSSE_PSS_TEST),
+)
+
+@testset "Test Serialization/De-Serialization PSID Tests" begin
+    system_catalog = SystemCatalog(SYSTEM_CATALOG)
+    for (name, descriptor) in system_catalog.data[PSIDTestSystems]
+        # build a new system from scratch
+        if haskey(PSID_BUILD_TESTS, name)
+            args = PSID_BUILD_TESTS[name]
+            for dyn_type in args[2]
+                sys =
+                    build_system(PSIDTestSystems, name; force_build = true, (args[1] => dyn_type))
+                @test isa(sys, System)
+                # build a new system from json
+                @test PSB.is_serialized("$(name)_$(dyn_type)", false, false)
+                sys2 = build_system(
+                    PSIDTestSystems,
+                    name;
+                    (args[1] => dyn_type),
+                )
+                @test isa(sys2, System)
+
+                PSB.clear_serialized_system(name)
+                @test !PSB.is_serialized(name, false, false)
+            end
+        else
+            sys = build_system(PSIDTestSystems, name; force_build = true)
+            @test isa(sys, System)
+            # build a new system from json
+            @test PSB.is_serialized(name, false, false)
+            sys2 = build_system(
+                PSIDTestSystems,
+                name;
+            )
+            @test isa(sys2, System)
+
+            PSB.clear_serialized_system(name)
+            @test !PSB.is_serialized(name, false, false)
+        end
+    end
+end
---
+++ test/test_psytestsystems.jl
@@ -0,0 +1,27 @@
+@testset "Test Serialization/De-Serialization PSY Tests" begin
+    system_catalog = SystemCatalog(SYSTEM_CATALOG)
+    for (name, descriptor) in system_catalog.data[PSYTestSystems]
+        # build a new system from scratch
+        for forecasts in [true, false], reserves in [true, false]
+            sys = build_system(PSYTestSystems, name;
+            add_forecasts = forecasts,
+            add_reserves = reserves,
+            force_build = true)
+
+            @test isa(sys, System)
+            # build a new system from json
+            @test PSB.is_serialized(name, forecasts, reserves)
+            sys2 = build_system(
+                PSYTestSystems,
+                name;
+                add_forecasts = forecasts,
+                add_reserves = reserves,
+                force_build = true,
+            )
+            @test isa(sys2, System)
+
+            PSB.clear_serialized_system(name)
+            @test !PSB.is_serialized(name, forecasts, reserves)
+        end
+    end
+end
