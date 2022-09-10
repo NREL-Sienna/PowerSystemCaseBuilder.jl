84290e1be7acbe56567aa7233ab92515aa1023e0
Rodrigo Henriquez <rodrigomha@gmail.com>
2022-09-09 17:46:34 -0700

    fix typo

--- src/library/psid_library.jl
+++ src/library/psid_library.jl
@@ -1,5 +1,5 @@
 # PSID cases creation
-function build_psid_test_4bus_multigen(; kwargs...)
+function build_psid_4bus_multigen(; kwargs...)
     sys_kwargs = filter_kwargs(; kwargs...)
     data_dir = get_raw_data(; kwargs...)
     raw_file = joinpath(data_dir, "FourBusMulti.raw")
@@ -9,5 +9,14 @@ function build_psid_test_4bus_multigen(; kwargs...)
     for l in get_components(PSY.PowerLoad, sys)
         PSY.set_model!(l, PSY.LoadModels.ConstantImpedance)
     end
+    return sys
+end
+
+function build_psid_11bus_andes(; kwargs...)
+    sys_kwargs = filter_kwargs(; kwargs...)
+    data_dir = get_raw_data(; kwargs...)
+    raw_file = joinpath(data_dir, "11BUS_KUNDUR.raw")
+    dyr_file = joinpath(data_dir, "11BUS_KUNDUR_TGOV.dyr")
+    sys = System(raw_file, dyr_file; sys_kwargs...)
     return sys
 end
