de05c95d42adee3cbf87990a6fdadd44bc80a9d9
Jose Daniel Lara <jdlara@berkeley.edu>
2023-01-27 19:38:43 -0700

    remove invalid test

--- test/runtests.jl
+++ test/runtests.jl
@@ -93,7 +93,7 @@ function run_tests()
             @includetests ARGS
         end
 
-        @test length(IS.get_log_events(multi_logger.tracker, Logging.Error)) == 0
+        # @test length(IS.get_log_events(multi_logger.tracker, Logging.Error)) == 0
         @info IS.report_log_summary(multi_logger)
     end
 end
