54786d9f60fcaadfec0228359ee27543cbf20d50
Jose Daniel Lara <jdlara@berkeley.edu>
2024-06-11 16:26:42 -0600

    update testdata references

--- src/definitions.jl
+++ src/definitions.jl
@@ -1,5 +1,5 @@
 const PACKAGE_DIR = joinpath(dirname(dirname(pathof(PowerSystemCaseBuilder))))
-const DATA_DIR = "/Users/rhenriqu/PSY_Renaming/PowerSystemsTestData"
+const DATA_DIR = joinpath(LazyArtifacts.artifact"CaseData", "PowerSystemsTestData-3.0-alpha3")
 
 const RTS_DIR = joinpath(LazyArtifacts.artifact"rts", "RTS-GMLC-0.2.2")
 
