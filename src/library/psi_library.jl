cdb1b9b88315b34fe8ab8d74344874ceb43b1b28
rodrigomha <rodrigomha@gmail.com>
2024-08-07 10:43:02 -0700

    fix kwarg

--- src/library/psi_library.jl
+++ src/library/psi_library.jl
@@ -1361,7 +1361,7 @@ function _duplicate_system(main_sys::PSY.System, twin_sys::PSY.System, HVDC_line
             active_power_limits_to = (min = -1000.0, max = 1000.0),
             reactive_power_limits_from = (min = -1000.0, max = 1000.0),
             reactive_power_limits_to = (min = -1000.0, max = 1000.0),
-            LinearCurve(0.1),
+            loss = PSY.LinearCurve(0.1),
             services = Vector{Service}[],
             ext = Dict{String, Any}(),
         )
