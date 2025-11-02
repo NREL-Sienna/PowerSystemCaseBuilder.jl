e40b6aac2482646261f0fdd0037a111fb6b3b278
Jose Daniel Lara <jdlara@berkeley.edu>
2025-11-02 16:01:20 -0700

    fixes for docs and formatter

--- src/definitions.jl
+++ src/definitions.jl
@@ -8,23 +8,23 @@ const SYSTEM_DESCRIPTORS_FILE = joinpath(PACKAGE_DIR, "src", "system_descriptor.
 const SERIALIZED_DIR = joinpath(PACKAGE_DIR, "data", "serialized_system")
 
 const SERIALIZE_FILE_EXTENSIONS =
-	[".json", "_metadata.json", "_validation_descriptors.json", "_time_series_storage.h5"]
+    [".json", "_metadata.json", "_validation_descriptors.json", "_time_series_storage.h5"]
 
 const ACCEPTED_PSID_TEST_SYSTEMS_KWARGS = [:avr_type, :tg_type, :pss_type, :gen_type]
 const AVAILABLE_PSID_PSSE_AVRS_TEST =
-	["AC1A", "AC1A_SAT", "EXAC1", "EXST1", "SEXS", "SEXS_noTE"]
+    ["AC1A", "AC1A_SAT", "EXAC1", "EXST1", "SEXS", "SEXS_noTE"]
 
 const AVAILABLE_PSID_PSSE_TGS_TEST = ["GAST", "HYGOV", "TGOV1"]
 
 const AVAILABLE_PSID_PSSE_GENS_TEST = [
-	"GENCLS",
-	"GENROE",
-	"GENROE_SAT",
-	"GENROU",
-	"GENROU_NoSAT",
-	"GENROU_SAT",
-	"GENSAE",
-	"GENSAL",
+    "GENCLS",
+    "GENROE",
+    "GENROE_SAT",
+    "GENROU",
+    "GENROU_NoSAT",
+    "GENROU_SAT",
+    "GENSAE",
+    "GENSAL",
 ]
 
 const AVAILABLE_PSID_PSSE_PSS_TEST = ["STAB1", "IEEEST", "IEEEST_FILTER"]
