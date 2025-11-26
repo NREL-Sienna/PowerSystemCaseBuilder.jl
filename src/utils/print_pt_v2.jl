a1e9bd5472e925d7ccea311b1a8b8b8d290fc75e
Chubin <hchubin@nrel.gov>
2025-11-26 13:51:33 -0800

    fixed a typo in this script and now all tests passing for when I'm using both PT versions 2 and 3

--- src/utils/print_pt_v2.jl
+++ src/utils/print_pt_v2.jl
@@ -2,7 +2,7 @@ function show_systems(sys::SystemCatalog, category::Type{<:SystemCategory}; kwar
     descriptors = get_system_descriptors(category, sys)
     sort!(descriptors; by = x -> x.name)
     header = ["Name", "Descriptor"]
-    data = Array{Any, 2}(undef, length(descriptors), length(column_labels))
+    data = Array{Any, 2}(undef, length(descriptors), length(header))
     for (i, d) in enumerate(descriptors)
         data[i, 1] = get_name(d)
         data[i, 2] = get_description(d)
